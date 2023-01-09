8171c64950255d55cdc737910a0ee9f43b3fd4de
Mizuri <pro.1.h@outlook.com>
2023-01-08 18:40:48 -0700

    adfded js

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,35 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
-CREATE DATABASE employeeTracker_db;
-USE employeeTracker_db;
+USE employeeTracker_db
 
-CREATE TABLE department(
-    department_id INTEGER AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(department_id)
-    );
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Engineering");
+INSERT INTO department (name) VALUES ("Finance");
+INSERT INTO department (name) VALUES ("Legal");
+INSERT INTO department (name) VALUES ("Management");
 
-CREATE TABLE role(
-    role_id INTEGER AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    PRIMARY KEY(role_id),
-        FOREIGN KEY (department_id)
-        REFERENCES department(department_id)
-    );
-    
-  CREATE TABLE employee(
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NULL,
-    manager_id INTEGER NULL,
-    PRIMARY KEY(id),
-        FOREIGN KEY(role_id)
-        REFERENCES role(role_id),
-    );
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 80000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer", 150000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 12000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Account Manager", 16000, 5);
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 125000, 6);
+INSERT INTO role (title, salary, department_id) VALUES ("Legal Team Lead", 250000, 7);
+INSERT INTO role (title, salary, department_id) VALUES ("Lawyer", 190000, 8);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("John", "Doe", 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("Mike", "Chan", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("Ashley", "Rodriguez", 3, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("Kevin", "Tupic", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("Kunal", "Sing", 5, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("Malia", "Brown", 6, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("Sarah", "Lourd", 7, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES ("Tom", "Allen", 8, 7);
+
+UPDATE employee SET manager_id=3 WHERE id=1;
+UPDATE employee SET manager_id=1 WHERE id=2;
+UPDATE employee SET manager_id=9 WHERE id=3;
+UPDATE employee SET manager_id=3 WHERE id=4;
+UPDATE employee SET manager_id=9 WHERE id=5;
+UPDATE employee SET manager_id=2 WHERE id=6;
+UPDATE employee SET manager_id=9 WHERE id=7;
+UPDATE employee SET manager_id=7 WHERE id=8;
+UPDATE employee SET manager_id=9 WHERE id=10;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,37 +1,31 @@
-
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
-INSERT INTO department (name)
-VALUES
-      ('Sales'),
-      ('Engineering'),
-      ('Finance'),
-      ('Legal');
+CREATE TABLE department(
+    department_id INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(department_id)
+    );
 
-INSERT INTO role 
-  (title, salary, department_id)
-VALUES 
-  ('Sales Lead, 100000, 1'),
-  ('Salesperson, 80000, 2'),
-  ('Lead Engineer, 150000, 3'),
-  ('Software Engineer, 12000, 4'),
-  ('Account Manager, 16000, 5'),
-  ('Accountant, 125000, 6'),
-  ('Legal Team Lead, 250000, 7'),
-  ('Lawyer, 190000, 8');
+CREATE TABLE role(
+    role_id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY(role_id),
+        FOREIGN KEY (department_id)
+        REFERENCES department(department_id)
+    );
+    
+  CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NULL,
+    manager_id INTEGER NULL,
+    PRIMARY KEY(id),
+        FOREIGN KEY(role_id)
+        REFERENCES role(role_id),
+    );
 
-  INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
-  VALUES 
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupic', 4, 3),
-    ('Kunal', 'Sing', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
-
-SELECT * FROM employeeTracker_db.department;
-SELECT * FROM employeeTracker_db.employee;
-SELECT * FROM employeeTracker_db.role;
