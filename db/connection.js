83c8f63e154d6a7b5b356f44d263a08516f5f257
Mizuri <pro.1.h@outlook.com>
2023-01-08 20:08:00 -0700

    added connection

--- db/connection.js
+++ db/connection.js
@@ -5,11 +5,8 @@ require("dotenv").config();
 const connection = mysql.createConnection(
 	{
 		host: process.env.MYSQL_HOST,
-
 		user: process.env.MYSQL_USER,
-
 		password: process.env.MYSQL_PW,
-
 		database: process.env.MYSQL_NAME,
 	},
 	console.log(`Connected to database.`)
--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const inquirer = require("inquirer");
 const connection = require("./db/connection");
 const cTable = require("console.table");
 
-// Connect to mysql server and database
+//Setup Database connection
+connection.connect(function (err) {
+	if (err) throw err;
+	console.log("User connected to tracker as id: " + connection.threadId + "\n");
+	questions();
+});
+
 questions();
